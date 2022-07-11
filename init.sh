8bdeba08b96b2fd7f40316f50220465e86ae2079
Nasty <bogatenkova.anastasiya@mail.ru>
2022-07-11 17:35:30 +0300

    make dataset and train model

---
+++ init.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/mindee/doctr
+
+# TODO download data
+
+cp train_pytorch.py doctr/references/detection/train_pytorch.py
+cp detection.py doctr/doctr/datasets/detection.py
---
+++ train.sh
@@ -0,0 +1,1 @@
+python3 doctr/references/detection/train_pytorch.py /Users/anastasiabogatenkova/work/TextDetection/data/train /Users/anastasiabogatenkova/work/TextDetection/data/val db_resnet50 --pretrained
